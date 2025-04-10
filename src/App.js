0e2d323766927997134e6a97330722edc3e51c50
srifinmis <misreports@srifincredit.com>
2025-04-10 17:32:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,82 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Topbar from './components/Topbar';
-// import Reports from './components/Reports';
-// import Reupload from './components/Reupload'; 
-// import LoanDetailsReport from './components/LoanDetailsReport';
-// import Lucreport from './components/LUCReport';
-// import BorrowerMasterReport from './components/BorrowMasterReport';
-// import CreditReport from './components/CreditReport';
-// import DeathReport from './components/DeathReport';
-// import EmployeeMasterReport from './components/EmployeeMasterReport';
-// import ForeClosureReport from './components/ForeClouserReport';
-// import LoanApplicationReport from './components/LoanApplicationReport';
-// import ReportsLogin from './components/Login'
-// const App = () => {
-//   return (
-//     <Router>
-//       <div style={{ display: 'flex', minHeight: '100vh' }}>
-
-//         {/* Fixed Sidebar */}
-//         <div style={{
-//           position: 'fixed', 
-//           top: 0, 
-//           left: 0, 
-//           bottom: 0, 
-//           width: '160px', 
-//           backgroundColor: '#fff', 
-//           zIndex: 100
-//         }}>
-//           <Navbar />
-//         </div>
-
-//         {/* Main Content Section */}
-//         <div style={{
-//           marginLeft: '240px', 
-//           paddingTop: '60px', 
-//           flexGrow: 1
-//         }}>
-
-//           {/* Fixed Topbar */}
-//           <div style={{
-//             position: 'fixed',
-//             top: 0,
-//             left: '240px',
-//             right: 0,
-//             backgroundColor: '#fff',
-//             zIndex: 101,
-//             width: 'calc(100% - 260px)', 
-//             height: '60px'
-//           }}>
-//             <Topbar />
-//           </div>
-
-//           {/* Page Content */}
-//           <div style={{ marginTop: '-40px', padding: '20px' }}>
-//             <Routes>
-//               <Route path="/" element={<ReportsLogin />} />
-//               <Route path="/components/Reports" element={<Reports />} />
-//               <Route path="/components/Reupload" element={<Reupload />} />
-//               <Route path="/components/LoanDetailsReport" element={<LoanDetailsReport />} />
-//               <Route path="/components/LUCReport" element={<Lucreport />} /> 
-//               <Route path="/components/BorrowMasterReport" element={<BorrowerMasterReport />} />
-//               <Route path="/components/CreditReport" element={<CreditReport />} />
-//               <Route path="/components/DeathReport" element={<DeathReport />} />
-//               <Route path="/components/EmployeeMasterReport" element={<EmployeeMasterReport />} />
-//               <Route path="/components/LoanApplicationReport" element={<LoanApplicationReport />} />
-//               <Route path="/components/ForeClouserReport" element={<ForeClosureReport />} />
-//             </Routes>
-//           </div>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 
@@ -141,7 +62,7 @@ const Layout = ({ children }) => {
             style={{
               position: "fixed",
               top: 0,
-              left: "240px",
+              left: "250px",
               right: 0,
               backgroundColor: "#fff",
               zIndex: 101,
